31dab0519bd32296c152f50793e32b506170f55e
Roland <alimaroland85@yahoo.com>
2018-04-29 16:44:38 +0200

    "  "

--- app/src/main/java/com/example/roland/rtpsteeamingtesttvapp/MainActivity.java
+++ app/src/main/java/com/example/roland/rtpsteeamingtesttvapp/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         }
         catch ( UnknownHostException | SocketException e ) {
             e.printStackTrace();
+            e.getMessage();
         }
 
     }
